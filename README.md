8f7e6cf12f9e2f5096b4abe2c40b5f42e43d93dd
eyedeekay <idk@mulder>
2024-03-30 11:23:44 -0400

    Initialize example

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # TLS-Somewhere
 A deeply cynical but probably safe attempt to provide a way to use TLS for hidden services without manually accepting self-signed certificates. Almost certainly won't see the light of day.
+
+The basic premise is to configure a browser to accept self-signed certificates by default, then configure a browser extension to reject them and present it's own messages instead.
+If the key for the TLS certificate could not have been created without the key for the hidden service, then no message should be presented at all.
+
+Credit:
+
+Example code provided by this Stack Exchange thread: https://stackoverflow.com/questions/2402121/within-a-web-browser-is-it-possible-for-javascript-to-obtain-information-about
---
+++ background.js
@@ -0,0 +1,27 @@
+var log = console.log.bind(console)
+
+log(`\n\nTLS browser extension loaded`)
+
+// https://developer.chrome.com/extensions/match_patterns
+var ALL_SITES = { urls: ['<all_urls>'] }
+
+// Mozilla doesn't use tlsInfo in extraInfoSpec 
+var extraInfoSpec = ['blocking']; 
+
+// https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/webRequest/onHeadersReceived
+browser.webRequest.onHeadersReceived.addListener(async function(details){
+    log(`\n\nGot a request for ${details.url} with ID ${details.requestId}`)
+
+    // Yeah this is a String, even though the content is a Number
+    var requestId = details.requestId
+
+    var securityInfo = await browser.webRequest.getSecurityInfo(requestId, {
+        certificateChain: true,
+        rawDER: false
+    });
+
+    log(`securityInfo: ${JSON.stringify(securityInfo, null, 2)}`)
+
+}, ALL_SITES, extraInfoSpec) 
+
+log('Added listener')
